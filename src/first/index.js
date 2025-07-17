cb85e80272f52719a3779c7d0e3d55e6193ac696
Squirrel <squirrelhomebrew@gmail.com>
2025-07-17 09:37:26 -0700

    Update index.js

--- src/first/index.js
+++ src/first/index.js
@@ -8,4 +8,8 @@ app.get('/', (req, res) => {
   res.render('index', { name: 'greg' });
 });
 
+app.get('/greg', (req, res) => {
+  res.render('greg');
+});
+
 app.listen(3000, () => console.log('server running on http://localhost:3000'));
