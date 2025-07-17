274df89c8d0bf37e4c04b56f2a876006a78e5856
Squirrel <squirrelhomebrew@gmail.com>
2025-07-17 09:20:51 -0700

    Create index.js

---
+++ src/first/views/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+  res.render('index', { name: 'greg' });
+});
+
+app.listen(3000, () => console.log('server running on http://localhost:3000'));
